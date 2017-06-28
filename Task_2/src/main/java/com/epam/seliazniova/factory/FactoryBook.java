bf7374fc12177867526372535a0b74a08054d664
Iryna_Seliazniova <tsarok94@gmail.com>
2017-06-28 13:36:19 +0300

    Task_2 second commit

---
+++ Task_2/src/main/java/com/epam/seliazniova/factory/FactoryBook.java
@@ -0,0 +1,9 @@
+package com.epam.seliazniova.factory;
+
+/**
+ * Created by Iryna_Seliazniova on 6/28/2017.
+ */
+public class FactoryBook extends PublicationFactory {
+    public FactoryBook() {
+    }
+}
